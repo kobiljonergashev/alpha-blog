126ae85543cff2234033bb5b034cf9f64236c740
Kobiljon Ergashev <kobiljonergashev@gmail.com>
2015-12-21 05:02:26 +0500

    Create categories model and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :update, :edit]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_admin, only: [:destroy]
+
 	def index
 		@users = User.all
 	end
@@ -10,19 +14,18 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = "Welcome to the Alpha Blog, #{@user.username}!"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			redirect_to 'new'
 		end
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully!"
 			redirect_to articles_path
@@ -32,12 +35,36 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:danger] = "User and all the articles created by the user have been deleted"
+		redirect_to users_path
 	end
 
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user and !current_user.admin?
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
+	end
+
+	def require_admin
+		if logged_in? and !current_user.admin?
+			flash[:danger] = "Only admin user can delete"
+			redirect_to root_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   resources :users, except: [:new]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
